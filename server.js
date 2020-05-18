ffcbb2e5f7bed666895f23760d7587e92921ea8f
jeffngugi <ngugimwathi254@gmail.com>
2020-05-18 17:48:27 +0300

    procfile

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const express = require("express");
 const http = require("http");
 const path = require("path");
 let app = express();
-app.use(express.static(path.join(__dirname, "build")));
+// app.use(express.static(path.join(__dirname, "build")));
+app.use("*", (req, res) =>
+  res.status(200).send({
+    message: "Not found, try to add /api/v1 to access the api",
+  })
+);
 const port = process.env.PORT || "9000";
 app.set("port", port);
 const server = http.createServer(app);
