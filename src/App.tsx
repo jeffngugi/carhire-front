8f7ad6244a5611b132c1f5e018a41b054fbe5372
jeffngugi <ngugimwathi254@gmail.com>
2021-11-08 16:58:59 +0300

    feat:responsive dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{FC}from 'react'
+import Button from '@mui/material/Button'
+import {Header} from './components'
+import { BrowserRouter as Router} from 'react-router-dom'
+import './App.css'
 
-function App() {
+
+const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Header />
+      </Router>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,154 @@
+import React, {FC, useState, useEffect} from 'react'
+import {AppBar, Box, Toolbar, IconButton, MenuItem, Typography, Button,Drawer, Link, makeStyles}  from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu';
+import {Link as RouterLink} from 'react-router-dom'
+import './Header.css'
+
+const headersData = [
+  {
+    label: "Home",
+    href: "/listings",
+  },
+  {
+    label: "Services",
+    href: "/mentors",
+  },
+  {
+    label: "Vehicles",
+    href: "/account",
+  },
+  {
+    label: "Blog",
+    href: "/logout",
+  },
+  {
+    label: "Contact",
+    href: "/logout",
+  },
+  {
+    label: "Sign In",
+    href: "/logout",
+  },
+];
+
+
+
+const Header:FC = () => {
+
+  const [state, setState] = useState({ mobileView: false, drawerOpen: false})
+
+  const {mobileView, drawerOpen} = state;
+
+
+  useEffect(() => {
+    const setResponsiveness = () => {
+      return window.innerWidth < 900
+        ? setState((prevState) => ({ ...prevState, mobileView: true }))
+        : setState((prevState) => ({ ...prevState, mobileView: false }));
+    };
+
+    setResponsiveness();
+    window.addEventListener("resize", () => setResponsiveness());
+
+    return () => {
+      window.removeEventListener("resize", () => setResponsiveness());
+    }
+  }, []);
+
+
+
+  const displayDesktop = ()=>{
+    return (<Toolbar className='toolbar'>
+            {appLogo}
+            {getMenuButtons()}
+            </Toolbar>)
+  }
+
+  const displayMobile = () => {
+    const handleDrawerOpen = () =>
+      setState((prevState) => ({ ...prevState, drawerOpen: true }));
+    const handleDrawerClose = () =>
+      setState((prevState) => ({ ...prevState, drawerOpen: false }));
+
+
+const getDrawerChoices = () => {
+  return headersData.map(({ label, href }) => {
+    return (
+      <Link
+        {...{
+          component: RouterLink,
+          to: href,
+          color: "inherit",
+          style: { textDecoration: "none" },
+          key: label,
+        }}
+      >
+        <MenuItem>{label}</MenuItem>
+      </Link>
+    );
+  });
+};
+
+    return (
+      <Toolbar>
+        <IconButton
+          {...{
+            edge: "start",
+            color: "inherit",
+            "aria-label": "menu",
+            "aria-haspopup": "true",
+            onClick:handleDrawerOpen,
+          }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Drawer
+          {...{
+            anchor: "left",
+            open: drawerOpen,
+            onClose: handleDrawerClose,
+          }}
+        >
+          <div className='drawerContainer'>{getDrawerChoices()}</div>
+        </Drawer>
+    <div>{appLogo}</div>
+    </Toolbar>
+      );
+    };
+    
+
+  const appLogo = (
+    <Typography variant="h6" component="h1" className='logo'>
+      GariNow
+    </Typography>
+  );
+
+  const getMenuButtons = () => {
+    return headersData.map(({ label, href }) => {
+      return (
+        <Button
+          {...{
+            key: label,
+            color: "inherit",
+            to: href,
+            component: RouterLink,
+            className:'menuButton'
+          }}
+        >
+          {label}
+        </Button>
+      );
+    });
+  };
+
+
+    return (
+      <header className='header'>
+        <AppBar className='header'>
+          {mobileView? displayMobile() : displayDesktop()}
+          </AppBar>
+      </header>
+    )
+}
+
+export default Header
